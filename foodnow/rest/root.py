402f4bb135ba402b919907a82ede643b48fdbf6a
Maddy Muscari <mmuscari@twilio.com>
2020-03-20 12:15:23 -0700

    add scraped pantries from foodnow website

--- foodnow/rest/root.py
+++ foodnow/rest/root.py
@@ -1,8 +1,10 @@
 from flask_restful import Resource
 from foodnow.db import get_redis_client
 
+
 class Root(Resource):
     def get(self):
         redis = get_redis_client()
         count = redis.incr('test_counter', 1)
         return {'Hit Counter': count}
+

aa0f1321bb6dedbe5a9d74aca3c6a9caeb0e6507
Maddy Muscari <mmuscari@twilio.com>
2020-03-21 15:12:10 -0700

    use heroku postgres database

--- foodnow/db/__init__.py
+++ foodnow/db/__init__.py
@@ -7,9 +7,10 @@ def get_postgres_client():
     database_url = os.environ.get("DATABASE_URL")
     parsed = parse.urlparse(database_url)
     split = str(parsed.netloc).split('@')
+    database = parsed.path[1:]
     auth = split[0]
     netloc = split[1]
     (user, passwd) = auth.split(':')
     (host, port) = netloc.split(':')
-    return psycopg2.connect(host=host, port=port, user=user, password=passwd, database='foodnow')
+    return psycopg2.connect(host=host, port=port, user=user, password=passwd, database=database)
 

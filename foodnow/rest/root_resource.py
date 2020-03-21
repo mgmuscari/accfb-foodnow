a80dba063dd1fcbdb1a9cf76bc345c140664e8c1
Maddy Muscari <mmuscari@twilio.com>
2020-03-21 15:07:00 -0700

    remove redis

--- foodnow/rest/root_resource.py
+++ foodnow/rest/root_resource.py
@@ -1,10 +1,8 @@
 from flask_restful import Resource
-from foodnow.db import get_redis_client
+from foodnow.db import get_postgres_client
 
 
 class RootResource(Resource):
     def get(self):
-        redis = get_redis_client()
-        count = redis.incr('test_counter', 1)
-        return {'Hit Counter': count}
+        return "boop"
 

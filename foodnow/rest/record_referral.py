8a66e2586f7406289b2213523c8a626d4cd0078a
Maddy Muscari <mmuscari@twilio.com>
2020-04-02 18:26:35 -0700

    timezone offset

--- foodnow/rest/record_referral.py
+++ foodnow/rest/record_referral.py
@@ -3,7 +3,8 @@ from foodnow.db import get_postgres_client
 from foodnow.db.postgres.distribution_site_dao import PostgresDistributionSiteDao
 from foodnow.db.postgres.referral_dao import PostgresReferralDao
 from foodnow.model.referral import Referral
-from datetime import date
+import datetime
+import pytz
 
 
 class RecordReferralResource(Resource):
@@ -11,13 +12,14 @@ class RecordReferralResource(Resource):
     def post(self):
         pgclient = get_postgres_client()
         try:
+            now = datetime.datetime.now(tz=pytz.timezone("America/Los_Angeles")).date()
             site_id = request.form.get('site_id', None)
             count = request.form.get('count', 1)
             if site_id is not None:
                 distribution_site_dao = PostgresDistributionSiteDao(pgclient)
                 site = distribution_site_dao.get_site(site_id)
                 if site is not None:
-                    referral = Referral(site, date.today(), count)
+                    referral = Referral(site, now, count)
                     referral_dao = PostgresReferralDao(pgclient)
                     referral_dao.increment_referral(referral)
         finally:

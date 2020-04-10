4d14e83a088ea3288e42479fcecfaac75a1368a9
Maddy Muscari <mmuscari@twilio.com>
2020-04-10 12:00:10 -0700

    log while reaping

--- foodnow/reap_records.py
+++ foodnow/reap_records.py
@@ -19,29 +19,35 @@ if __name__ == '__main__':
     today = datetime.datetime.now()
     last_week = today - datetime.timedelta(days=7)
 
+    log.info("Deleting executions")
     try:
         for execution in client.studio.flows.get(flow_sid).executions.list(date_created_from=last_week):
             try:
                 if execution.status != ExecutionInstance.Status.ACTIVE:
+                    log.info("Delete execution {}".format(execution.sid))
                     execution.delete()
             except Exception:
                 log.exception("An exception occurred deleting an execution")
     except Exception:
         log.exception("An exception occurred iterating executions")
 
+    log.info("Deleting messages")
     try:
         for message in client.messages.list(date_sent_after=last_week):
             try:
+                log.info("Delete message {}".format(message.sid))
                 message.delete()
             except Exception:
                 log.exception("An exception occurred deleting a message")
     except Exception:
         log.exception("An exception occurred iterating messages")
 
+    log.info("Deleting calls")
     try:
         for call in client.calls.list(start_time_after=last_week):
             try:
                 if call.status not in [call.Status.IN_PROGRESS, call.Status.RINGING, call.status.QUEUED]:
+                    log.info("Delete call {}".format(call.sid))
                     call.delete()
             except Exception:
                 log.exception("An exception occurred deleting a call")

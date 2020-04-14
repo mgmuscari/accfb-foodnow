b993810b2e907f0f450e8b0613eb60a99a1eacce
Maddy Muscari <mmuscari@twilio.com>
2020-04-13 17:10:22 -0700

    debug url and content

--- foodnow/app.py
+++ foodnow/app.py
@@ -46,6 +46,8 @@ def verify_request():
             validator = RequestValidator(twilio_auth_token)
             url = request.url
             content = request.form
+            logging.debug(url)
+            logging.debug(content)
             if not validator.validate(url, content, twilio_signature):
                 abort(401)
         except Exception as e:
